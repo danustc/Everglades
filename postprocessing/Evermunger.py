c279f708a1545f5430e349e7e21a7acdc3e4f669
danustc <xdlaway@gmail.com>
2018-12-27 22:27:44 -0800

    1227_Home-0

--- postprocessing/Evermunger.py
+++ postprocessing/Evermunger.py
@@ -16,12 +16,34 @@ transform_mat = np.array([
     [-sq2, 0., sq2]
     ]) # the transform matrix between the lab frame and the image frame
 
-def transform(stack, scan_range = 6.0, pxl = 0.103):
+def _phase_construct_(NK, x0, shift = True):
+    kk = np.arange(NK)
+    if shift:
+        kk = np.fft.fftshift(kk)
+    phi = 2.0*np.pi*kk*x0/NK
+    phase = np.cos(phi) - np.sin(phi)*1j
+    return phase
+
+def shift_fourier(img, dy, dx = 0.):
+    '''
+    shift the image in Fourier space
+    '''
+    NY, NX = img.shape
+    yp = _phase_construct_(NY, dy)
+    xp = _phase_construct_(NX, dx)
+    phase_img = np.outer(yp,xp)
+    fimg = np.fft.ifftshift(np.fft.fftshift(np.fft.fft2(img)) * phase_img)
+    img_shifted = np.fft.ifft2(fimg)
+
+    return np.abs(img_shifted)
+
+def transform(stack, scan_range = 6.0, pxl = 0.103, method = 'direct'):
     '''
     img: the raw image acquired
     scan_range: the range of Z-scanning, unit micron.
     pxl: pixel size, unit micron.
     have a zero-padded stack.
+    method: 'direct' or 'fourier'
     '''
     NZ, NY, NX = stack.shape
     yy = np.arange(NY)*pxl
@@ -29,12 +51,18 @@ def transform(stack, scan_range = 6.0, pxl = 0.103):
     padded_stack = np.zeros((NZ, NY + 2*n_pad, NX))
     DXB = np.linspace(0., scan_range, NZ) # The scan range
     z_range = DXB/np.sqrt(2.)
-    y_range = -z_range
-    for nn in range(NZ):
-        y_shift = int(z_range[NZ-nn-1]/pxl)
-        print('y_shift:', y_shift)
-        padded_stack[nn, y_shift:(y_shift + NY), :] = stack[nn]
+    #y_range = -z_range
 
+    if method == 'direct':
+        for nn in range(NZ):
+            y_shift = int(z_range[NZ-nn-1]/pxl)
+            print('y_shift:', y_shift)
+            padded_stack[nn, y_shift:(y_shift + NY), :] = stack[nn]
+    elif method == 'fourier':
+        for nn in range(NZ):
+            y_shift = int(z_range[NZ-nn-1]/pxl)
+            padded_slice = np.pad(stack[nn], ((n_pad, n_pad),(0,0)), 'constant')
+            padded_stack[nn] = shift_fourier(padded_slice, y_shift)
     # next, shift the y back
     return padded_stack
 
@@ -46,9 +74,9 @@ def main():
     '''
     SC_list = glob.glob(global_datapath + '*SC*.npy')
     SC_psf = np.load(SC_list[0])
-    padded_stack = transform(SC_psf).astype('uint16')
+    padded_stack = transform(SC_psf, method = 'fourier').astype('uint16')
 
-    tf.imsave('padded.tif', padded_stack)
+    tf.imsave('padded_ft.tif', padded_stack)
     tf.imsave('original.tif', SC_psf.astype('uint16'))
 
 if __name__ == '__main__':
