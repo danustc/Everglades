934f7b48b7f8d969e269ab9cb0db7825f486b28e
danustc <xdlaway@gmail.com>
2019-02-03 12:42:38 -0800

    0203_Byers-2

---
+++ madCityLabs/nanodrive/nanodrive_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+
+from ctypes import *
+
+
+class API:
+    
+    def __init__(self):
+	# Load library:
+        self.mcl = windll.LoadLibrary("C:\\Program Files\\Mad City Labs\\NanoDrive\\Madlib")
+	# Define the return types of some the functions
+        self.mcl.MCL_GetCalibration.restype = c_double
+        self.mcl.MCL_SingleReadN.restype = c_double
+	# Get stage handle:
+        self.handle = self.mcl.MCL_InitHandle()
+
+    def getCalibration(self,axis):
+        return self.mcl.MCL_GetCalibration(c_ulong(axis),self.handle)
+
+    def singleReadN(self,axis):
+        pos = self.mcl.MCL_SingleReadN(c_ulong(axis), self.handle)
+        return pos
+
+    def singleWriteN(self,position,axis):
+        self.mcl.MCL_SingleWriteN(c_double(position),c_ulong(axis),self.handle)
+
+    def releaseHandle(self):
+        self.mcl.MCL_ReleaseHandle(self.handle)
